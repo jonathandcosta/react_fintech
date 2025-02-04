9409e3434716959fb2f83b6e0ce77f8c7d224a05
Jonathan Costa <jonathanduartecosta@gmail.com>
2025-02-04 16:11:59 -0300

    feat: page sale id

--- src/Pages/Venda.tsx
+++ src/Pages/Venda.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { useParams } from "react-router-dom"
+import useFetch from "../Hooks/useFetch"
+import { IVenda } from "../Context/DataContext"
+
 
 const Venda = () => {
+  const { id } = useParams()
+  const { data } = useFetch<IVenda>(
+    `https://data.origamid.dev/vendas/${id}`
+  )
+
+  if (data === null) return null
   return (
     <div>
-      Venda
+      <div className="box mb">ID: {data.id}</div>
+      <div className="box mb">Nome: {data.nome}</div>
+      <div className="box mb">Preço: {data.preco.toLocaleString('pt-br', {
+        style: 'currency',
+        currency: 'BRL'
+      })}
+      </div>
+      <div className="box mb">Status: {data.status}</div>
+      <div className="box mb">Pagamento: {data.pagamento}</div>
     </div>
   )
 }
