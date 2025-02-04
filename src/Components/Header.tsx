51b56641439207c299c8290c083142f52c7ca19c
Jonathan Costa <jonathanduartecosta@gmail.com>
2025-02-04 15:30:19 -0300

    feat: effect in the header name

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
-import { useData } from '../Context/DataContext';
 import DateRange from './DateRange';
 import Meses from './Meses';
+import { useLocation } from 'react-router-dom';
 
 const Header = () => {
   const [title, setTitle] = React.useState('Resumo')
+
+  const location = useLocation()
+  React.useEffect(() => {
+    if (location.pathname === '/') {
+      setTitle('Resumo')
+    } else if (location.pathname === '/vendas') {
+      setTitle('Vendas')
+    }
+  }, [location])
 
   return (
     <header className='mb'>
